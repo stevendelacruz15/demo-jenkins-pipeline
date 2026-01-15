99c3d9fb1761fbd0ca928e64bc7f798df8dbb1d7
Stevow <stevow@SDCs-Macbook-Pro.local>
2026-01-14 23:09:59 -0500

    2nd commit

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "primuslearning-app-steve"
+    bucket = "terraform-backend-jenkins-pipeline-sdc123"
     region = "us-east-1"
     key = "eks/terraform.tfstate"
   }
